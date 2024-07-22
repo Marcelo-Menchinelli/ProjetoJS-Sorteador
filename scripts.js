f2212e4a314887449f406570dcd93b9bec8a37a6
Marcelo-Menchinelli <menchinelli77@gmail.com>
2024-07-22 10:59:28 -0300

    Atualização de Projeto_Sorteador. Acrescimo de um input para mostrar o resultado.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
-function gerenateNumber() {
-   
-   const min = Math.ceil(document.querySelector (".input-min").value) 
 
-   const max = Math.floor(document.querySelector (".input-max").value) 
+
+const inputResult = document.querySelector(".input-result")
+
+const buttonSorteio = document.querySelector(".button-sorteio")
+
+
+function gerenateNumber() {
+
+   const inputResult = document.querySelector(".input-result").value
+
+   const min = Math.ceil(document.querySelector(".input-min").value)
+
+   const max = Math.floor(document.querySelector(".input-max").value)
 
    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
+   inputResult.innerHTML = result
+
+   inputResult.value = result
+
+   console.log(result)
+   
    alert(result)
 }
+
+buttonSorteio.addEventListener("click", gerenateNumber)
